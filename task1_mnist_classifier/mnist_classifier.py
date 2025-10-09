09d0f3d364371c678b8a14f1cf2ab93addae3b5c
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-10-09 12:50:40 +0300

    Refactor MnistClassifier initialization to simplify model instantiation

--- task1_mnist_classifier/mnist_classifier.py
+++ task1_mnist_classifier/mnist_classifier.py
@@ -1,16 +1,16 @@
-from task1_mnist_classifier.models.cnn_classifier import CnnClassifier
-from task1_mnist_classifier.models.nn_classifier import NnClassifier
-from task1_mnist_classifier.models.random_forest_classifier import RandomForestClassifier
-from task1_mnist_classifier.mnist_classifier_interface import MnistClassifierInterface
+from models.cnn_classifier import CnnClassifier
+from models.nn_classifier import NnClassifier
+from models.random_forest_classifier import RandomForestClassifier
+from mnist_classifier_interface import MnistClassifierInterface
 
 class MnistClassifier(MnistClassifierInterface):
-    def __init__(self, algorithm, model):
-        if algorithm == 'cnn':
-            self.classifier = CnnClassifier(model)
-        elif algorithm == 'rf':
-            self.classifier = RandomForestClassifier(model)
-        elif algorithm == 'nn':
-            self.classifier = NnClassifier(model)
+    def __init__(self, algorithm):
+        if algorithm == "rf":
+            self.classifier = RandomForestClassifier()
+        elif algorithm == "nn":
+            self.classifier = NnClassifier(None)
+        elif algorithm == "cnn":
+            self.classifier = CnnClassifier(None)
         else:
             raise ValueError(f"Unknown algorithm: {algorithm}")
 
