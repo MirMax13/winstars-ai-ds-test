7336a8c99dc4c0b6083784940987f5ca9fdfc839
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-10-09 12:22:57 +0300

    Refactor NnClassifier to initialize model architecture and implement training and prediction methods

--- task1_mnist_classifier/models/nn_classifier.py
+++ task1_mnist_classifier/models/nn_classifier.py
@@ -1,11 +1,19 @@
-from task1_mnist_classifier.mnist_classifier_interface import MnistClassifierInterface
+from mnist_classifier_interface import MnistClassifierInterface
+import tensorflow as tf
 
 class NnClassifier(MnistClassifierInterface):
     def __init__(self, model):
-        self.model = model
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(10, activation='softmax')
+        ])
+        self.model.compile(optimizer='adam',
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['accuracy'])
 
     def train(self, train_data, train_labels):
-        pass
+        self.model.fit(train_data, train_labels, epochs=5)
 
     def predict(self, test_data):
-        pass
+        return self.model.predict(test_data)
