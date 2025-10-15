8394184ddbd2913c7e9cd864f8a6a83f2471adbf
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-10-15 19:19:08 +0300

    Handle warning with input layer

--- task1/models/nn_classifier.py
+++ task1/models/nn_classifier.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 class NnClassifier(MnistClassifierInterface):
     def __init__(self, model):
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Input(shape=(28, 28)),
+            tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
