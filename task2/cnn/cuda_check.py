b12b220b9e28cd4b8ee37cc69bd5213d04737ce0
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-10-15 20:52:53 +0300

    Remove unused TensorFlow files and dataset splitting script

--- task2/cnn/cuda_check.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-print("TensorFlow version:", tf.__version__)
-print("Built with CUDA:", tf.test.is_built_with_cuda())
-print("GPU devices:", tf.config.list_physical_devices('GPU'))
-
-from tensorflow.python.client import device_lib
-print("\nAll devices:")
-print(device_lib.list_local_devices())
